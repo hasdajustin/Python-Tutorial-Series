e48524cd75c26a016f7250eef41bf5050a6619ef
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-04 06:29:17 +0600

    Operators

---
+++ 06-Operators/arithmetic-operators.py
@@ -0,0 +1,7 @@
+a, b = 10, 3
+print(a + b)  # Addition: 13
+print(a - b)  # Subtraction: 7
+print(a * b)  # Multiplication: 30
+print(a / b)  # Division: 3.33
+print(a // b)  # Floor Division: 3
+print(a ** b)  # Exponent: 10^3 = 1000
