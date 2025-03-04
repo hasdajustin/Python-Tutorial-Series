99fd9132a02791461bcd309344d1c4e4cae542e5
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-04 06:30:07 +0600

    Function

---
+++ 09-Functions/function.py
@@ -0,0 +1,10 @@
+def my_function():
+  print("Hello from a function")
+
+my_function() # Output: Hello from a function
+
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Matheus"))  # Output: Hello, Matheus!
