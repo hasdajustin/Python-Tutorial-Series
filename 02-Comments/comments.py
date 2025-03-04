1abe1f52d8e9408a7ae2e88290cfe0be5c186cba
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-04 06:08:01 +0600

    Variables & Data Type
    Python is dynamically typed, so you don’t need to declare a variable type.

---
+++ 02-Comments/comments.py
@@ -0,0 +1,6 @@
+#This is my single line comment
+
+'''
+This is a multi-line
+comment
+'''
---
+++ 03-Variables and Data Types/variables-DataTypes.py
@@ -0,0 +1,6 @@
+name = "Matheus Justin Hasda" # string
+age = 24 # integer
+height = 5.6 # float
+is_student = True #boolean
+
+print(name, age, height, is_student)
