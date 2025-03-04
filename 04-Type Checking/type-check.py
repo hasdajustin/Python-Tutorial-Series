5159954329b0a5258e68dc2107d9fa6e69d34cd1
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-04 06:17:38 +0600

    Type Checking

---
+++ 04-Type Checking/type-check.py
@@ -0,0 +1,12 @@
+name = "Matheus Justin Hasda" # string
+age = 24 # integer
+height = 5.6 # float
+is_student = True #boolean
+
+print(name, age, height, is_student)
+
+#Type Checking
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
