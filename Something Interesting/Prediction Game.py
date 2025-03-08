2353c5dbd35db77a037012aa9dfc2b37ab04a301
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-09 00:39:34 +0600

    Prediction game!

---
+++ Something Interesting/Prediction Game.py
@@ -0,0 +1,24 @@
+import random
+
+def prediction_game():
+    print("Welcome to the Number Prediction Game!")
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    
+    while True:
+        try:
+            guess = int(input("Guess a number between 1 and 100: "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+    prediction_game()
