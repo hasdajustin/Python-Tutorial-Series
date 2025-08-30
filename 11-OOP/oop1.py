94e820360f4bc9de8e05c4e5c8fe9f9a5d17277f
Amrito Ben Hasda <matheusjustinhasda@gmail.com>
2025-08-31 01:44:48 +0600

    add a method which is find out the car age

--- 11-OOP/oop1.py
+++ 11-OOP/oop1.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Car:
     def __init__(self, brand, model, year):
         self.brand = brand
@@ -7,5 +9,11 @@ class Car:
     def car_info(self):
         print(f'Brand:{self.brand}, Model: {self.model}, Year: {self.year}')
 
+    def car_age(self):
+        current_year = date.today().year
+        age = current_year - self.year
+        print(f'Car age: {age}')
+
 car1 = Car('Toyota', 'Premio', 2017)
 car1.car_info()
+car1.car_age()
